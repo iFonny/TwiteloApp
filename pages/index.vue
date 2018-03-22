a0e40544925e1d1144d68a686342e2ad76bd30bb
Nicolas Thouchkaieff <thouch_n@epitech.eu>
2018-03-22 19:05:44 +0900

    Integration games list

--- pages/index.vue
+++ pages/index.vue
@@ -58,6 +58,8 @@
 </template>
 
 <script>
+import VueNotifications from "vue-notifications";
+
 import Examples from "~/components/home/Examples";
 import GamesList from "~/components/home/GamesList";
 
@@ -66,37 +68,36 @@ export default {
     Examples,
     GamesList
   },
-  methods: {
-    async logout() {
-      await this.$store.dispatch("logout");
-    }
-  },
-  data() {
-    return {
-      gamesList: [
-        {
-          icon: "/public/images/game/lol/icon.png",
-          id: "lol",
-          image: "/public/images/game/lol/image.png",
-          name: "League Of Legends",
-          small_name: "LoL"
-        },
-        {
-          icon: "/public/images/game/speedrun/icon.png",
-          id: "speedrun",
-          image: "/public/images/game/speedrun/image.png",
-          name: "Speedrun.com",
-          small_name: "Speedrun"
-        }
-      ],
-      latestUsers: [
+  async asyncData({ app }) {
+    let gamesList = [];
+    let latestUsers = [];
+    try {
+      gamesList = (await app.$axios.$get("/api/game/min")).data;
+      latestUsers = [
         {
           username: "iFonny_",
           pp:
             "http://pbs.twimg.com/profile_images/963649016004272129/VhbWWXT__normal.jpg" // TODO: remplacer '_400x400' par '_normal' server-side
         }
-      ]
-    };
+      ]; // TODO: REQUEST LATEST USERS
+    } catch (e) {
+      gamesList = [];
+      latestUsers = [];
+    }
+
+    return { gamesList, latestUsers };
+  },
+  methods: {
+    async logout() {
+      await this.$store.dispatch("logout");
+    }
+  },
+  notifications: {
+    showNotification: {
+      type: VueNotifications.types.success,
+      title: "Default",
+      message: "That's the success!"
+    }
   }
 };
 </script>
