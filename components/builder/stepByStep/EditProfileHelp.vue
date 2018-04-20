b38aea86558b5db616370c80c1600e0709d67bee
Nicolas Thouchkaieff <thouch_n@epitech.eu>
2018-04-20 16:37:01 +0900

    mode assisté neuneu

---
+++ components/builder/stepByStep/EditProfileHelp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="step-content-help">
+
+    <p class="title is-size-4">{{$t('builder.step-by-step.help.autoupdater-switch')}}</p>
+    <p><img src="/images/help/autoupdater-switch.gif" /></p>
+
+    <br><br>
+
+    <p class="title is-size-4">{{$t('builder.step-by-step.help.move-data')}}</p>
+    <p><img src="/images/help/move-data.gif" /></p>
+
+    <br><br>
+
+    <p class="title is-size-4">{{$t('builder.step-by-step.help.add-existing-data')}}</p>
+    <p><img src="/images/help/add-existing-data.gif" /></p>
+
+    <br><br>
+
+    <p class="title is-size-4">{{$t('builder.step-by-step.help.edit-data')}}</p>
+    <p><img src="/images/help/edit-data.gif" /></p>
+
+    <br><br>
+
+    <p class="title is-size-4">{{$t('builder.step-by-step.help.delete-data')}}</p>
+    <p><img src="/images/help/delete-data.gif" /></p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+.step-content-help {
+  padding: 1.5rem;
+  background-color: #313131;
+}
+</style>
---
+++ components/builder/stepByStep/SelectTagsHelp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="step-content-help">
+
+    <p class="title is-size-4">{{$t('builder.step-by-step.help.add-data')}}</p>
+    <p><img src="/images/help/add-data.gif" /></p>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+.step-content-help {
+  padding: 1.5rem;
+  background-color: #313131;
+}
+</style>
